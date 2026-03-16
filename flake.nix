3305f5f81aa8aa0d4dbb4f34389bcf313f9e5983
ErikDev5 <voducphuc572@gmail.com>
2026-03-16 11:07:26 +0700

    feat(flake): add flake configuration for dev environment

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_24
+            pkgs.python312
+            pkgs.git
+          ];
+
+          shellHook = ''
+            echo "Dev environment ready"
+            node --version
+            python --version
+            git --version
+          '';
+        };
+      });
+}
